c8caaaa3dce308467d78d3b1a83b2a1fa87fcabf
Aalleepphh <118466315+Aalleepphh@users.noreply.github.com>
2023-09-04 16:12:26 +0200

    Create uitloggen.php

---
+++ herhalingsoefening/uitloggen.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    include "connect.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Uitloggen</title>
+</head>
+<body>
+    <h1>Log uit</h1>
+    
+    <?php
+        session_destroy();
+        echo 'Succesvol uitgelogd.';
+    ?>
+
+</body>
+</html>
