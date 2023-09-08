e698155773035f20d24479eb59aa2574596b1ddd
Aalleepphh <118466315+Aalleepphh@users.noreply.github.com>
2023-09-08 15:16:09 +0200

    Create boek_toevoegen.php

---
+++ herhalingsoefening/boek_toevoegen.php
@@ -0,0 +1,55 @@
+<?php
+    session_start();
+    include "connect.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boek toevoegen</title>
+</head>
+<body>
+
+    <?php
+        if (isset($_POST["knop"])){
+            $naam = $_POST["naam"];
+            $prijs = $_POST["prijs"];
+            $type = $_POST["type"];
+
+            $sql = 'INSERT INTO tblboek(naam, prijs, type) VALUES ('.$naam.', '.$prijs.', '.$type.')';
+            $resultaat = $mysqli->query($sql);
+
+            if ($resultaat){
+                echo '
+                    <h1>Succes</h1><br>
+                    <p>Boek succesvol toegevoegd, klik <a href="boeken_toevoegen.php">hier</a> om terug te gaan.</p>
+                ';
+            } else {
+                echo '
+                    <h1>Mislukking</h1><br>
+                    <p>Error boek toevoegen, '.$mysqli->error.'</p>
+                ';
+            }
+        } else {
+
+            echo '
+                <h1>Voeg boek toe</h1><br>
+                <form method="post" action="boek_wijzigen.php">
+                    Titel: <input type="text" name="naam" required>
+                    <br>
+                    Prijs: <input type="number" step="0.01" name="prijs" required>
+                    <br>
+                    Type: <select name="type">
+                        <option value="huur">huur</option>
+                        <option value="koop">koop</option>
+                    </select>
+                    <br><br>
+                    <input type="submit" name="knop" value="Voeg toe">
+                </form>
+            ';
+        }
+    ?>
+</body>
+</html>
