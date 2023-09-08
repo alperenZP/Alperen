94ac14278236e02e6b14b53d1b57f8e847cca839
Aalleepphh <118466315+Aalleepphh@users.noreply.github.com>
2023-09-08 14:19:12 +0200

    Update boek_verwijderen.php

--- herhalingsoefening/boek_verwijderen.php
+++ herhalingsoefening/boek_verwijderen.php
@@ -15,20 +15,33 @@
     <?php
         
 
-        $sql = 'DELETE FROM tblboek WHERE boeknummer = '.$boeknummer.'';
+        $sql = 'SELECT *, count(*) AS aantal FROM tblboekinklas WHERE boeknummer = '.$_GET["te_verwijderen"].'';
         $resultaat = $mysqli->query($sql);
+        $row = $resultaat->fetch_assoc();
 
-        if ($resultaat){
-            echo '
-                <h1>Succes</h1><br>
-                <p>Boek succesvol verwijderd, klik <a href="boeken_bekijken.php">hier</a> om terug te gaan.</p>
-            ';
+        if ($row["aantal"] == 0){
+            $sql = 'DELETE FROM tblboek WHERE boeknummer = '.$boeknummer.'';
+            $resultaat = $mysqli->query($sql);
+
+            if ($resultaat){
+                echo '
+                    <h1>Succes</h1><br>
+                    <p>Boek succesvol verwijderd, klik <a href="boeken_bekijken.php">hier</a> om terug te gaan.</p>
+                ';
+            } else {
+                echo '
+                    <h1>Mislukking</h1><br>
+                    <p>Error boek verwijderen, '.$mysqli->error.'</p>
+                ';
+            }
         } else {
             echo '
-                <h1>Mislukking</h1><br>
-                <p>Error boek verwijderen, '.$mysqli->error.'</p>
+                <h1>Mag niet verwijderen</h1>
+                <p>Deze boek is al in gebruik. Je mag hij niet verwijderen.</p>
             ';
         }
+
+        
         
     ?>
 </body>
